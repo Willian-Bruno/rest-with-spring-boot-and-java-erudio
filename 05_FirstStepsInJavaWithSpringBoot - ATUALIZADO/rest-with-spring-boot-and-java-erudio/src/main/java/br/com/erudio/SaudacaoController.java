7e0a917d33e8c06578a1bd5caa29819f92dc0f1d
Willbr Gamer <brunowilliam942@gmail.com>
2025-01-01 18:31:43 -0300

    Fist Steps in Java with SpringBoot

---
+++ 05_FirstStepsInJavaWithSpringBoot - ATUALIZADO/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/SaudacaoController.java
@@ -0,0 +1,19 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class SaudacaoController {
+	private static final String template = "Hello, %s! Tudo bem?";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/saudacao")
+	public Saudacao saudacao ( @RequestParam(value = "pessoa", defaultValue = "World") String pessoa) {
+		
+		return new Saudacao(counter.incrementAndGet(), String.format(template, pessoa));
+	}
+}
