1153f402a7b3001cc7532133402d630c0c1f55c8
Willbr Gamer <brunowilliam942@gmail.com>
2025-01-01 21:58:19 -0300

    First Steps in Java with Spring Boot 4

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/ClasseTeste.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/ClasseTeste.java
@@ -2,8 +2,10 @@ package br.com.erudio;
 
 public class ClasseTeste {
 	public String teste;
+	public String teste1;
 	
-	public ClasseTeste (String teste) {
+	public ClasseTeste (String teste, String teste1) {
 		this.teste = teste;
+		this.teste1 = teste1;
 	}
 }
